75a48babb77bc5cc3941848a20f7440c0d8d8099
tamaina <tamaina@hotmail.co.jp>
2023-05-03 12:38:56 +0000

    test1

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,36 +1,39 @@
 import * as _ from '../lib/index';
 import { describe, test } from '@jest/globals';
-import { expectAssignable, expectType } from 'tsd';
+import { expectAssignable, expectNotAssignable, expectType } from 'tsd';
 
 describe('SchemaType', () => {
     describe('types', () => {
-        test('null', () => {
-            type S = _.SchemaType<{ type: 'null' }, []>;
-            expectType<S>(null);
-        });
         test('string', () => {
             type S = _.SchemaType<{ type: 'string' }, []>;
             expectAssignable<S>('string');
+            expectNotAssignable<S>(123);
         });
         test('number', () => {
             type S = _.SchemaType<{ type: 'number' }, []>;
             expectAssignable<S>(123);
+            expectNotAssignable<S>('string');
         });
         test('boolean', () => {
             type S = _.SchemaType<{ type: 'boolean' }, []>;
             expectAssignable<S>(true);
+            expectNotAssignable<S>('string');
         });
         test('null', () => {
             type S = _.SchemaType<{ type: 'null' }, []>;
+            expectType<S>(null);
             expectAssignable<S>(null);
+            expectNotAssignable<S>('string');
         });
         test('array', () => {
             type S = _.SchemaType<{ type: 'array'; items: { type: 'string' } }, []>;
             expectAssignable<S>(['string']);
+            expectNotAssignable<S>('string');
         });
         test('object', () => {
             type S = _.SchemaType<{ type: 'object'; properties: { foo: { type: 'string' } } }, []>;
             expectAssignable<S>({ foo: 'string' });
+            expectNotAssignable<S>('string');
         });
     });
 });
