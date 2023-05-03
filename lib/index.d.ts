bcbf488baaada034e5062893db1356b1706d7618
tamaina <tamaina@hotmail.co.jp>
2023-05-03 17:37:30 +0000

    wip

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -17,22 +17,16 @@ type OfSchema = {
 
 export type Obj = Record<string, JSONSchema7>;
 
-type ProhibitInfinitRoop<s extends Obj, Ks extends ReadonlyArray<keyof s>, R extends JSONSchema7[]> =
-	Ks extends readonly [infer A, ...infer B] ? B extends ReadonlyArray<keyof s> ? A extends keyof s ?
-		s[A]['$ref'] extends R[number]['$id'] ?
-			Extract<R[number], { type: 'object' | 'array'; $id: s[A]['$ref']; }> extends JSONSchema7 ?
-				ProhibitInfinitRoop<s, B, R>
-			: [A, ...ProhibitInfinitRoop<s, B, R>]
-		: [A, ...ProhibitInfinitRoop<s, B, R>]
-	: never : never : [];
+type ProhibitInfinitRoop<s extends Obj, K extends keyof s, R extends JSONSchema7[], T extends JSONSchema7 = s[K]> =
+	Extract<R[number], { $id: T['$ref']; type: 'object' }> extends never ? K : never;
 
 // https://github.com/misskey-dev/misskey/issues/8535
 // To avoid excessive stack depth error,
 // deceive TypeScript with UnionToIntersection (or more precisely, `infer` expression within it).
 export type ObjType<s extends Obj, RequiredProps extends ReadonlyArray<keyof s>, R extends JSONSchema7[]> =
 	UnionToIntersection<
-		{ -readonly [P in keyof s]?: ChildSchemaType<s[P], R> } &
-		{ -readonly [Q in ProhibitInfinitRoop<s, RequiredProps, R>[number]]-?: ChildSchemaType<s[Q], R> }
+		{ -readonly [P in keyof s]?: ChildSchemaType<s[P], R> } |
+		{ -readonly [Q in ProhibitInfinitRoop<s, RequiredProps[number], R>]-?: ChildSchemaType<s[Q], R> }
 	>;
 
 // https://qiita.com/ssssota/items/7e05f05b57e71dfe1cf9
